9f01f7814922cc70a778b5b108afdc092635db0c
tomerdb <tomerdb@gmail.com>
2025-02-28 12:07:26 +0200

    added removeMatchStats method

--- app/src/main/java/com/example/footballresults/utils/StatisticsCalculator.java
+++ app/src/main/java/com/example/footballresults/utils/StatisticsCalculator.java
@@ -20,6 +20,53 @@ public class StatisticsCalculator {
         this.teamStatsDao = new TeamStatsDao(context);
     }
 
+    public void removeMatchStats(Match match) {
+        matchDao.open();
+        teamStatsDao.open();
+
+        // Get team stats
+        TeamStats teamAStats = teamStatsDao.getTeamStatsByName(match.getTeamA());
+        TeamStats teamBStats = teamStatsDao.getTeamStatsByName(match.getTeamB());
+
+        if (teamAStats != null && teamBStats != null) {
+            // Update matches played
+            teamAStats.setMatchesPlayed(teamAStats.getMatchesPlayed() - 1);
+            teamBStats.setMatchesPlayed(teamBStats.getMatchesPlayed() - 1);
+
+            // Update goals
+            teamAStats.setGoalsScored(teamAStats.getGoalsScored() - match.getTeamAGoals());
+            teamAStats.setGoalsAgainst(teamAStats.getGoalsAgainst() - match.getTeamBGoals());
+            teamBStats.setGoalsScored(teamBStats.getGoalsScored() - match.getTeamBGoals());
+            teamBStats.setGoalsAgainst(teamBStats.getGoalsAgainst() - match.getTeamAGoals());
+
+            // Update wins, draws, losses, and points
+            if (match.getTeamAGoals() > match.getTeamBGoals()) {
+                // Team A won
+                teamAStats.setWins(teamAStats.getWins() - 1);
+                teamAStats.setPoints(teamAStats.getPoints() - 3);
+                teamBStats.setLosses(teamBStats.getLosses() - 1);
+            } else if (match.getTeamAGoals() < match.getTeamBGoals()) {
+                // Team B won
+                teamBStats.setWins(teamBStats.getWins() - 1);
+                teamBStats.setPoints(teamBStats.getPoints() - 3);
+                teamAStats.setLosses(teamAStats.getLosses() - 1);
+            } else {
+                // Draw
+                teamAStats.setDraws(teamAStats.getDraws() - 1);
+                teamBStats.setDraws(teamBStats.getDraws() - 1);
+                teamAStats.setPoints(teamAStats.getPoints() - 1);
+                teamBStats.setPoints(teamBStats.getPoints() - 1);
+            }
+
+            // Save updated stats
+            teamStatsDao.updateTeamStats(teamAStats);
+            teamStatsDao.updateTeamStats(teamBStats);
+        }
+
+        matchDao.close();
+        teamStatsDao.close();
+    }
+
     public void calculateStatsForMatch(Match match) {
         matchDao.open();
         teamStatsDao.open();
